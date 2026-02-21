078a27a12841121d53259446061dc8df1c08bd1e
MLAbram <23126920+MLAbram@users.noreply.github.com>
2026-02-21 14:54:00 -0500

    Updated PHP

--- mlabram-enhanced-floating-admin-bar/mlabram-enhanced-floating-admin-bar.php
+++ mlabram-enhanced-floating-admin-bar/mlabram-enhanced-floating-admin-bar.php
@@ -1,16 +1,9 @@
 <?php
 /*
-Plugin Name
-@package            MLAbram - Enhanced Floating Admin Bar
-@author             MLAbram
-@copyright          2023 MLAbram
-@License            GNU General Public License v3
-
-@wordpress-plugin
 Plugin Name:        MLAbram - Enhanced Floating Admin Bar
 Plugin URI:         https://mlabram.com
-Description:        Enhance the default WordPress floating admin bar.
-Version:            1.0.1
+Description:        Enhance the default WordPress floating admin bar with quick-access shortcuts.
+Version:            1.1.0
 Requires At Least:  5.0
 Requires PHP:       7.0
 Author:             MLAbram
@@ -23,43 +16,41 @@ Network:            true
 Update URI:         mlabram-enhanced-floating-admin-bar
 */
 
-/*
-MLAbram - Enhanced Floating Admin Bar is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
-
-MLAbram - Enhanced Floating Admin Bar is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with MLAbram - Enhanced Floating Admin Bar. 
-If not, see https://www.gnu.org/licenses/gpl-3.0.html.
-*/
-
-// create posts link in the floating admin bar
-function mlabram_enhanced_floating_admin_bar_add_posts($admin_bar) {
-	$args = array(
-        'parent' => 'site-name',
-	    'id' => 'posts',
-        'title' => 'Posts', 
-        'href' => '/wp-admin/edit.php'
-    );
-    $admin_bar->add_node($args);
+// Prevent direct access to the file
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
 }
-add_action('admin_bar_menu', 'mlabram_enhanced_floating_admin_bar_add_posts', 1998);
 
-// create pages link in the floating admin bar
-function mlabram_enhanced_floating_admin_bar_add_pages($admin_bar) {
-    $args = array(
+/**
+ * Adds "Posts" and "Pages" shortcuts to the "Site Name" menu in the Admin Bar.
+ *
+ * @param WP_Admin_Bar $admin_bar The Unified Admin Bar object.
+ */
+function mlabram_enhanced_floating_admin_bar_add_nodes( $admin_bar ) {
+    
+    // Only show these links to users who have permission to edit content
+    if ( ! current_user_can( 'edit_posts' ) ) {
+        return;
+    }
+
+    // Add 'Posts' link
+    $admin_bar->add_node( array(
         'parent' => 'site-name',
-	    'id' => 'pages',
-        'title' => 'Pages', 
-        'href' => '/wp-admin/edit.php?post_type=page'
-    );
-    $admin_bar->add_node($args);
+        'id'     => 'mlabram-posts',
+        'title'  => __( 'Posts', 'mlabram-enhanced-floating-admin-bar' ),
+        'href'   => admin_url( 'edit.php' ),
+        'meta'   => array( 'title' => __( 'Manage Posts', 'mlabram-enhanced-floating-admin-bar' ) )
+    ) );
+
+    // Add 'Pages' link
+    $admin_bar->add_node( array(
+        'parent' => 'site-name',
+        'id'     => 'mlabram-pages',
+        'title'  => __( 'Pages', 'mlabram-enhanced-floating-admin-bar' ),
+        'href'   => admin_url( 'edit.php?post_type=page' ),
+        'meta'   => array( 'title' => __( 'Manage Pages', 'mlabram-enhanced-floating-admin-bar' ) )
+    ) );
 }
-add_action('admin_bar_menu', 'mlabram_enhanced_floating_admin_bar_add_pages', 1999);
-?>
+
+// Hook into admin_bar_menu with a high priority to ensure it appears after standard items
+add_action( 'admin_bar_menu', 'mlabram_enhanced_floating_admin_bar_add_nodes', 999 );
